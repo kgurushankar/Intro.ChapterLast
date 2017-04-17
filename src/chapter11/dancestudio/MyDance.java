78e90a947b67fa30b9e9b4f9a4d9ec483e0d6fdb
kgurushankar773 <kgurushankar773@PR28.hhs.win.fuhsd.org>
2017-04-17 15:11:28 -0700

    Created New Dance for project and Removed Hopper from main

---
+++ src/chapter11/dancestudio/MyDance.java
@@ -0,0 +1,31 @@
+package chapter11.dancestudio;
+
+public class MyDance extends AbstractDance
+{
+  private static DanceStep[] steps = 
+    {
+      new DanceStep(0,  50,   0, 0,    0,   0,   0, 0),
+      new DanceStep(0,   0,   0, 0,    0,  50,  40, 0),
+      new DanceStep(0,   0,  40, 0,    0,   0,   0, 0),
+      new DanceStep(0,   0,   0, 0,    0, -50,   0, 0),
+      new DanceStep(0, -50, -40, 0,    0,   0,   0, 0),
+      new DanceStep(0,   0,   0, 0,    0,   0, -40, 0)
+    };
+
+  private static int[] rhythm = {1, 2, 2, 1, 2, 2};
+
+  public MyDance()
+  {
+    super(steps, rhythm);
+  }
+
+  public String getName()
+  {
+    return "Waltz";
+  }
+
+  public int getTempo()
+  {
+    return 700;
+  }
+}
